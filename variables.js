3ecd463cdd846ac6615ef160ea7b18bcfca6178b
CarlosSoto92 <carlos.soto.santis@gmail.com>
2021-09-25 13:29:11 -0500

    acabamos la clase de variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Carlos";
+
+// declarar
+var edad;
+
+//inicializar
+edad =28;
+
+// Variable con datos tipo Array declarador e inicializado
+var elementos = ["computadora", "Celular" ,]
+
+//Variable con datos tipo objeto
+var persona = {
+     nombre: "Carlos",
+     edad: 28
+}
+
+
