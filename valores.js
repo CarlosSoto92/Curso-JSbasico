cdba03847421df1619616a2718d8cf289da592d1
CarlosSoto92 <carlos.soto.santis@gmail.com>
2021-09-25 11:04:34 -0500

    acabamos la primera clase del curso basico de JS  -Valores-

---
+++ valores.js
@@ -0,0 +1,16 @@
+40  //dato tipo numero//
+
+"Carlos Soto" //dato tipo string o cadena de texto//
+
+true  //Dato booleano//
+false  // Dato booleano para validacion de cosas//
+
+null   // Valor vacio //
+undefined  // Valor vacio o placeholder que son para reservar//
+            //un lugar para que otro en el futuro lo pueda ocupar//}
+
+            //Valores tipo objetos//
+[1,2,3]  //Valor tipo Array o arreglo//
+
+{nombre: Carlos}  //Valor tipo objeto//
+
