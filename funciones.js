65ab9cc3d2ce134ad2e710b18722c39abc5c7b83
CarlosSoto92 <carlos.soto.santis@gmail.com>
2021-09-26 21:45:58 -0500

    empezamos la clase de funciones

---
+++ funciones.js
@@ -0,0 +1,26 @@
+
+//2 tipos de funciones//
+//Declarativas
+//Function es una palabra reservada para JS
+
+
+function miFuncion() {
+        return 3
+}
+
+
+
+//Expresión o anonimas (Que son creadas dentro de una variable//
+
+var miFuncion = function(a,b) {
+
+
+    return a + b;
+}
+  //Para utilizar las funciones hay que invocarlas o llamarlas//
+
+ //las declarativas las llamamos con el nombre de esta
+
+ miFuncion();
+
+///las expresivas se invocan con el nombre de la variable
