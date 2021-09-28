063501c7ab8dfc87096e11258d2b593d71dcad55
CarlosSoto92 <carlos.soto.santis@gmail.com>
2021-09-27 23:38:23 -0500

    actualizamos la clase de varibles

--- variables.js
+++ variables.js
@@ -1,12 +1,15 @@
-var nombre = "Carlos";
+
 
 // declarar
-var edad;
+var edad;narles un valor
 
-//inicializar
-edad =28;
+//inicializar es asignarles un valor
+edad =28; // Variable de dato tipo numero
 
-// Variable con datos tipo Array declarador e inicializado
+//Podemos declarar e inicializar una variable desde 1 sola linea o sentencia de codigo
+var nombre = "Carlos";  //Variable con dato tipó primario string
+
+// Ejemplo de Variable con datos tipo Array declaradora e inicializada
 var elementos = ["computadora", "Celular" ,]
 
 //Variable con datos tipo objeto
