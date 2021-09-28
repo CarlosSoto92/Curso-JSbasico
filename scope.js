2715d1e70caec414aea0913d58428c67a7402d5b
CarlosSoto92 <carlos.soto.santis@gmail.com>
2021-09-27 23:51:45 -0500

    argegamos unos detalles a la clase de scope

--- scope.js
+++ scope.js
@@ -3,7 +3,7 @@
 var miNombre = "Carlos"
 
 function nombre(){
-    //Una variable local solo es util dentro de la funcion.
+    //Una variable con Scope local solo es util dentro de la funcion.
     var miApellido = "Soto"
     console.log (miNombre + " " + miApellido)
 }
